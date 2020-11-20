d19ce0f8517cabe7ed5975d14ff2e275d9cf957d
Nikhil Nagrale <nknagrale634@gmail.com>
2020-11-21 01:00:18 +0530

    1486. XOR Operation in an Array

--- Easy/1486.XOR_Operation_in_an_Array.cpp
+++ Easy/1486.XOR_Operation_in_an_Array.cpp
@@ -8,3 +8,11 @@ public:
         return sum;
     }
 };
+
+/*
+Another O(1) solution
+https://leetcode.com/problems/xor-operation-in-an-array/discuss/697849/O(1)-time
+
+Another O(1) solution
+https://leetcode.com/problems/xor-operation-in-an-array/discuss/796508/O(1)-time
+*/

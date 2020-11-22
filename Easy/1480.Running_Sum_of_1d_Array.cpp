f7c77cf168bd99e340858ec08900844f2bf582f1
Nikhil Nagrale <nknagrale634@gmail.com>
2020-11-22 15:59:33 +0530

    1480. Running Sum of 1d Array

--- Easy/1480.Running_Sum_of_1d_Array.cpp
+++ Easy/1480.Running_Sum_of_1d_Array.cpp
@@ -1,12 +1,29 @@
-class Solution {
+// Time complexity - O(n)
+// Space comlexity - O(n)  (because I didn't do it in place) (we can easily make it O(1))
+class Solution
+{
 public:
-    vector<int> runningSum(vector<int>& nums) {
-        int sum=0;
+    vector<int> runningSum(vector<int> &nums)
+    {
+        int sum = 0;
         vector<int> ans;
-        for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
+        for (int i = 0; i < nums.size(); i++)
+        {
+            sum += nums[i];
             ans.push_back(sum);
         }
         return ans;
+    }
+};
+
+// Solution Using STL
+// Partial Sum Method in 
+class Solution
+{
+public:
+    vector<int> runningSum(vector<int> &nums)
+    {
+        partial_sum(nums.begin(), nums.end(), nums.begin());
+        return nums;
     }
 };

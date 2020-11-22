6478ce2388f034cdb6a9f76181b7336f6e64eb5a
Nikhil Nagrale <nknagrale634@gmail.com>
2020-11-22 16:24:40 +0530

    1108. Defanging an IP Address

--- Easy/1108.Defanging_an_IP_Address.cpp
+++ Easy/1108.Defanging_an_IP_Address.cpp
@@ -1,14 +1,35 @@
-class Solution {
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+// Best Solution
+class Solution
+{
 public:
-    string defangIPaddr(string address) {
+    string defangIPaddr(string address)
+    {
         string ans;
-        for(int i=0;i<address.length();i++){
-            if(address[i]=='.'){
-                ans+="[.]";
-            }else{
-                ans+=address[i];
+        for (int i = 0; i < address.length(); i++)
+        {
+            if (address[i] == '.')
+            {
+                ans += "[.]";
+            }
+            else
+            {
+                ans += address[i];
             }
         }
         return ans;
+    }
+};
+
+// Using regex expression
+// Worst Time Complexity + Space Complexity too
+// Worst solution
+class Solution
+{
+public:
+    string defangIPaddr(string address)
+    {
+        return regex_replace(address, regex("[.]"), "[.]");
     }
 };

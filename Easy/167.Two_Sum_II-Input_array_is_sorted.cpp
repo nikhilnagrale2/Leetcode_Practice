6b471a654093d72b4992fbe6c902c80760e3f07f
Nikhil Nagrale <nknagrale634@gmail.com>
2020-11-22 20:15:16 +0530

    167. Two Sum II - Input array is sorted

--- Easy/167.Two_Sum_II-Input_array_is_sorted.cpp
+++ Easy/167.Two_Sum_II-Input_array_is_sorted.cpp
@@ -1,3 +1,6 @@
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
 class Solution
 {
 public:
@@ -23,3 +26,44 @@ public:
 
 //can be done using binary search (nlogn)
 //https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/discuss/942235/C%2B%2B-Two-Pointers-or-O(n)-and-Binary-Search-or-O(n-log-n)
+
+//  Time Complexity - O(nlogn)
+//  Space Complexity - O(1)
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &numbers, int target)
+    {
+        vector<int> sums;
+        for (int i = 0; i < numbers.size(); i++)
+        {
+            int low = 0, high = numbers.size() - 1;
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+                if (numbers[mid] == target - numbers[i])
+                {
+                    if (mid == i)
+                    {
+                        low = mid + 1;
+                    }
+                    else
+                    {
+                        sums = {i + 1, mid + 1};
+                        return sums;
+                    }
+                }
+                else if (numbers[mid] < target - numbers[i])
+                {
+                    low = mid + 1;
+                }
+                else
+                {
+                    high = mid - 1;
+                }
+            }
+        }
+        return sums;
+    }
+};

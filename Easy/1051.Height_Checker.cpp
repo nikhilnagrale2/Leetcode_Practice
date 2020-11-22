1829e432fef9af6730d5663628a1ecfbfe619805
Nikhil Nagrale <nknagrale634@gmail.com>
2020-11-22 19:17:46 +0530

    1051. Height Checker

--- Easy/1051.Height_Checker.cpp
+++ Easy/1051.Height_Checker.cpp
@@ -1,13 +1,49 @@
-class Solution {
+//  Time Complexity - O(nlogn)
+//  Space Complexity - O(n)
+
+class Solution
+{
 public:
-    int heightChecker(vector<int>& heights) {
-        int sum=0;
+    int heightChecker(vector<int> &heights)
+    {
+        int sum = 0;
         vector<int> beforeh;
-        beforeh=heights;
-        sort(heights.begin(),heights.end());
-        for(int i=0;i<heights.size();i++)
-            if(heights[i]!=beforeh[i])
+        beforeh = heights;
+        sort(heights.begin(), heights.end());
+        for (int i = 0; i < heights.size(); i++)
+            if (heights[i] != beforeh[i])
                 sum++;
         return sum;
+    }
+};
+
+//  Optimized Solution
+//  Time Complexity - O(n)  // almost
+//  Space Complexity - O(n)
+
+class Solution
+{
+public:
+    int heightChecker(vector<int> &heights)
+    {
+        vector<int> HeightFreq(101);
+
+        for (int x : heights)
+            HeightFreq[x]++;
+
+        int result = 0;
+        int curHeight = 0;
+
+        for (int i = 0; i < heights.size(); i++)
+        {
+            while (HeightFreq[curHeight] == 0)
+                curHeight++;
+
+            if (curHeight != heights[i])
+                result++;
+
+            HeightFreq[curHeight]--;
+        }
+        return result;
     }
 };

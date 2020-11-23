ad98bacaa16d6588ba8d720ccf1673e12c86c1c4
Nikhil Nagrale <nknagrale634@gmail.com>
2020-11-23 10:34:36 +0530

    26. Remove Duplicates from Sorted Array

---
+++ Easy/26.Remove_Duplicates_from_Sorted_Array.cpp
@@ -0,0 +1,55 @@
+//  Naive Solution
+//  Time Complexity - O(n+no. of duplicates)
+//  Space Complexity -  O(1)
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        for (int i = 1; i < nums.size(); i++)
+        {
+            if (nums[i] == nums[i - 1])
+            {
+                nums.erase(nums.begin() + i);
+                i--;
+            }
+        }
+        return nums.size();
+    }
+};
+
+// Optimized Using C++ STL
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        nums.erase(unique(nums.begin(), nums.end()), nums.end());
+        return nums.size();
+    }
+};
+
+//  Most Optimized Solution Using Two Pointer Approach
+//  Time Complexity - O(n)
+//  Space Complexity - O(1)
+
+class Solution
+{
+public:
+    int removeDuplicates(vector<int> &nums)
+    {
+        if (nums.size() == 0)
+            return 0;
+        int p1 = 0;
+        for (int p2 = 1; p2 < nums.size(); p2++)
+        {
+            if (nums[p1] != nums[p2])
+            {
+                nums[p1 + 1] = nums[p2];
+                p1++;
+            }
+        }
+        return p1 + 1;
+    }
+};

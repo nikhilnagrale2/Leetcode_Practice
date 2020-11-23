7e6448fabd52c2d1e482fde7cb3925bca97a3f50
Nikhil Nagrale <nknagrale634@gmail.com>
2020-11-23 09:10:03 +0530

    1295. Find Numbers with Even Number of Digits

--- Easy/1295.Find_Numbers_with_Even_Number_of_Digits.cpp
+++ Easy/1295.Find_Numbers_with_Even_Number_of_Digits.cpp
@@ -34,3 +34,20 @@ public:
         return ans;
     }
 };
+
+// But its not fast.
+// above two solutions are much faster
+class Solution
+{
+public:
+    int findNumbers(vector<int> &nums)
+    {
+        int ans = 0;
+        for (auto x : nums)
+        {
+            if (to_string(x).length() % 2 == 0)
+                ans++;
+        }
+        return ans;
+    }
+};

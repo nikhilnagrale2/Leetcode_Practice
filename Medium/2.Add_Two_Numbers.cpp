40172417f7aa029f54698beb8845cfa25e9b57bd
Nikhil Nagrale <nknagrale634@gmail.com>
2021-04-23 14:55:51 +0530

    2.Add_Two_Numbers

---
+++ Medium/2.Add_Two_Numbers.cpp
@@ -0,0 +1,51 @@
+/*
+  Problem Link    :   https://leetcode.com/problems/add-two-numbers/
+
+  Author          :   Nikhil Nagrale
+  Codeforces      :   https://codeforces.com/profile/nikhilnagrale2
+  Codechef        :   https://www.codechef.com/users/nikhilnagrale2
+  Github          :   https://github.com/nikhilnagrale2
+  Leetcode        :   https://leetcode.com/nikhilnagrale2
+
+  Copyright       :   Study and Solve. :)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+//  Optimal Solution
+//  Time Complexity - O(n+m)
+//  Space Complexity - O(n+m)
+class Solution {
+   public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = new ListNode();
+        ListNode* temp = head;
+        int carry = 0;
+        while (l1 || l2 || carry) {
+            int sum = 0;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            sum += carry;
+            carry = sum / 10;
+            ListNode* node = new ListNode(sum % 10);
+            temp->next = node;
+            temp = temp->next;
+        }
+        return head->next;
+    }
+};

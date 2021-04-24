453c6477169459f4cdb6e03d97dcc1acc2cf758a
Nikhil Nagrale <nknagrale634@gmail.com>
2021-04-24 14:13:35 +0530

    104.Maximum_Depth_of_Binary_Tree

---
+++ Easy/104.Maximum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,61 @@
+/*
+  Problem Link    : https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+  Author          :   Nikhil Nagrale
+  Codeforces      :   https://codeforces.com/profile/nikhilnagrale2
+  Codechef        :   https://www.codechef.com/users/nikhilnagrale2
+  Github          :   https://github.com/nikhilnagrale2
+  Leetcode        :   https://leetcode.com/nikhilnagrale2
+
+  Copyright       :   Study and Solve. :)
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+//  DFS Solution - Recursive
+//  Time Complexity - O(nodes)
+//  Space Complexity - O(nodes) - implicit stack
+class Solution {
+   public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        int lh = maxDepth(root->left);
+        int rh = maxDepth(root->right);
+        return 1 + max(lh, rh);
+    }
+};
+
+//  BFS - Level Order Solution
+//  Time Complexity - O(nodes)
+//  Space Complexity - O()
+class Solution {
+   public:
+    int maxDepth(TreeNode* root) {
+        if (!root) return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int depth = 0;
+        while (!q.empty()) {
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+            depth++;
+        }
+        return depth;
+    }
+};

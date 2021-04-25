b26d166e9ebb750a283ff91c0c9fa8926c79f7cb
Nikhil Nagrale <nknagrale634@gmail.com>
2021-04-25 15:30:40 +0530

    236.Lowest_Common_Ancestor_of_Binary_Tree

---
+++ Medium/236.Lowest_Common_Ancestor_of_Binary_Tree.cpp
@@ -0,0 +1,44 @@
+/*
+  Problem Link    :
+  https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+
+  Author          :   Nikhil Nagrale
+  Codeforces      :   https://codeforces.com/profile/nikhilnagrale2
+  Codechef        :   https://www.codechef.com/users/nikhilnagrale2
+  Github          :   https://github.com/nikhilnagrale2
+  Leetcode        :   https://leetcode.com/nikhilnagrale2
+
+  Copyright       :   Study and Solve. :)
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+//  DFS Recursive
+//  Time Complexity - O(nodes)
+//  Space Complexity - O(nodes)
+class Solution {
+   public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (!root || root == p || root == q) return root;
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+        if (!left)
+            return right;
+        else if (!right)
+            return left;
+        else
+            return root;
+    }
+};
+
+//  Iterative
+//  Time Complexity - O(nodes)
+//  Space Complexity - O(nodes)

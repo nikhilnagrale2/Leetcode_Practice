8f63a4dfd011461e0e6f33f33d6349808dec7a4c
Nikhil Nagrale <nknagrale634@gmail.com>
2021-04-25 19:10:53 +0530

    98. Validate Binary Search Tree

--- Medium/98.Validate_Binary_Search_Tree.cpp
+++ Medium/98.Validate_Binary_Search_Tree.cpp
@@ -1,5 +1,5 @@
 /*
-  Problem Link    :   
+  Problem Link    :   https://leetcode.com/problems/validate-binary-search-tree/
 
   Author          :   Nikhil Nagrale
   Codeforces      :   https://codeforces.com/profile/nikhilnagrale2
@@ -9,3 +9,58 @@
 
   Copyright       :   Study and Solve. :)
 */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+
+//  Recursive DFS
+//  Time Complexity - O(nodes)
+//  Space Complexity - O(nodes)
+class Solution {
+   public:
+    bool check(TreeNode* root, long left, long right) {
+        if (!root) return true;
+        int value = root->val;
+        return value > left && value < right &&
+               check(root->left, left, value) &&
+               check(root->right, value, right);
+    }
+
+    bool isValidBST(TreeNode* root) { return check(root, LONG_MIN, LONG_MAX); }
+};
+
+//  Iterative Inorder DFS
+//  Time Complexity - O(nodes)
+//  Space Complexity - O(nodes)
+class Solution {
+   public:
+    bool isValidBST(TreeNode* root) {
+        long pre = LONG_MIN;
+        stack<TreeNode*> stk;
+        while (root || !stk.empty()) {
+            while (root) {
+                stk.push(root);
+                root = root->left;
+            }
+            root = stk.top();
+            stk.pop();
+            if (root->val <= pre) return false;
+            pre = root->val;
+            root = root->right;
+        }
+        return true;
+    }
+};
+
+//  Without inf value
+// * https://leetcode.com/problems/validate-binary-search-tree/discuss/32104/C%2B%2B-in-order-traversal-and-please-do-not-rely-on-buggy-INT_MAX-INT_MIN-solutions-any-more

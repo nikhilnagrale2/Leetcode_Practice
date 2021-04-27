3ab89ff7f7a75b96bc9b51468d33e55471e63db6
Nikhil Nagrale <nknagrale634@gmail.com>
2021-04-27 20:21:29 +0530

    538. Convert BST to Greater Tree

--- Medium/538.Convert_BST_to_Greater_Tree.cpp
+++ Medium/538.Convert_BST_to_Greater_Tree.cpp
@@ -23,9 +23,9 @@
  * };
  */
 
-//  DFS Inorder Recursive
+//  DFS ReverseInorder Recursive
 //  Time Complexity - O(n)
-//  Space Complexity- O(logn)
+//  Space Complexity- O(n)
 class Solution {
    public:
     int reverseInorder(TreeNode* root, int sum) {
@@ -38,6 +38,63 @@ class Solution {
 
     TreeNode* convertBST(TreeNode* root) {
         reverseInorder(root, 0);
+        return root;
+    }
+};
+
+//  DFS ReverseInorder Iterative
+//  Time Complexity - O(n)
+//  Space Complexity- O(n)
+class Solution {
+   public:
+    TreeNode* convertBST(TreeNode* root) {
+        stack<TreeNode*> stk;
+        TreeNode* curr = root;
+        int sum = 0;
+        while (curr || !stk.empty()) {
+            while (curr) {
+                stk.push(curr);
+                curr = curr->right;
+            }
+            curr = stk.top();
+            stk.pop();
+            sum += curr->val;
+            curr->val = sum;
+            curr = curr->left;
+        }
+        return root;
+    }
+};
+
+//  Morris ReverseInorder Traversal
+//  Time Complexity - O(n)
+//  Space Complexity - O(1)
+class Solution {
+   public:
+    TreeNode* convertBST(TreeNode* root) {
+        TreeNode* curr = root;
+        int sum = 0;
+        while (curr) {
+            if (curr->right) {
+                TreeNode* predecessor = curr->right;
+                while (predecessor->left != curr && predecessor->left != NULL) {
+                    predecessor = predecessor->left;
+                }
+                if (predecessor->left) {
+                    predecessor->left = NULL;
+                    sum += curr->val;
+                    curr->val = sum;
+                    curr = curr->left;
+                } else {
+                    predecessor->left = curr;
+                    curr = curr->right;
+                }
+            } else {
+                sum += curr->val;
+                curr->val = sum;
+                curr = curr->left;
+            }
+        }
         return root;
     }
 };

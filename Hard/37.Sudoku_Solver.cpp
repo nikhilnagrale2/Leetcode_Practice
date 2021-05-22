fb253579a6282169dc5f23f0d902cf16175a89e2
Nikhil Nagrale <nknagrale634@gmail.com>
2021-05-22 23:53:32 +0530

    37. Sudoku Solver

---
+++ Hard/37.Sudoku_Solver.cpp
@@ -0,0 +1,47 @@
+/*
+  Problem Link    :   https://leetcode.com/problems/sudoku-solver/
+
+  Author          :   Nikhil Nagrale
+  Codeforces      :   https://codeforces.com/profile/nikhilnagrale2
+  Codechef        :   https://www.codechef.com/users/nikhilnagrale2
+  Github          :   https://github.com/nikhilnagrale2
+  Leetcode        :   https://leetcode.com/nikhilnagrale2
+
+  Copyright       :   Study and Solve. :)
+*/
+
+//  BackTracking
+class Solution {
+   public:
+    bool isValid(int row, int col, vector<vector<char>>& board, char c) {
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == c) return false;
+            if (board[i][col] == c) return false;
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+                return false;
+        }
+        return true;
+    }
+
+    bool solve(vector<vector<char>>& board) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(i, j, board, c)) {
+                            board[i][j] = c;
+                            if (solve(board))
+                                return true;
+                            else
+                                board[i][j] = '.';
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    void solveSudoku(vector<vector<char>>& board) { solve(board); }
+};

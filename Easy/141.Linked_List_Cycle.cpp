0fa1e6079ec8444df4d8f7903a856b52426e8d0a
Nikhil Nagrale <nknagrale634@gmail.com>
2021-05-24 12:06:33 +0530

    141. Linked List Cycle

--- Easy/141.Linked_List_Cycle.cpp
+++ Easy/141.Linked_List_Cycle.cpp
@@ -47,7 +47,7 @@ class Solution {
         while (fast && fast->next) {
             slow = slow->next;
             fast = fast->next->next;
-            if (slow == fast) return fast;
+            if (slow == fast) return true;
         }
         return false;
     }

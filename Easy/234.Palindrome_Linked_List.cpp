6e317d3886fb8d7cd1f720508addf335a44671a9
Nikhil Nagrale <nknagrale634@gmail.com>
2021-05-24 13:44:25 +0530

    234. Palindrome Linked List

--- Easy/234.Palindrome_Linked_List.cpp
+++ Easy/234.Palindrome_Linked_List.cpp
@@ -1,5 +1,5 @@
 /*
-  Problem Link    :   
+  Problem Link    :   https://leetcode.com/problems/palindrome-linked-list/
 
   Author          :   Nikhil Nagrale
   Codeforces      :   https://codeforces.com/profile/nikhilnagrale2
@@ -22,25 +22,20 @@
  */
 
 //  Bruteforce Solution
-//  Time Complexity - O(n)
-//  Space Complexity - O(1)
-class Solution
-{
-public:
-    bool isPalindrome(vector<int> &p)
-    {
+//  Time Complexity - O(n)+O(n)
+//  Space Complexity - O(n)
+class Solution {
+   public:
+    bool isPalindrome(vector<int> &p) {
         bool isP = true;
         for (int i = 0; i < p.size() / 2; i++)
-            if (p[i] != p[p.size() - i - 1])
-                isP = false;
+            if (p[i] != p[p.size() - i - 1]) isP = false;
         return isP;
     }
 
-    bool isPalindrome(ListNode *head)
-    {
+    bool isPalindrome(ListNode *head) {
         vector<int> p;
-        while (head)
-        {
+        while (head) {
             p.push_back(head->val);
             head = head->next;
         }
@@ -48,6 +43,42 @@ public:
     }
 };
 
-//  Recursive  -->  https://leetcode.com/problems/palindrome-linked-list/discuss/181453/Java-or-5-lines-method-or-2ms-or-O(n)-or-recursive-or-easy-to-understand
+//  Recursive  -->
+//  https://leetcode.com/problems/palindrome-linked-list/discuss/181453/Java-or-5-lines-method-or-2ms-or-O(n)-or-recursive-or-easy-to-understand
 
-//  TODO: O(1) space
+//  Time Complexity - O(n)
+//  Space Complexity -  O(1) space
+class Solution {
+   public:
+    ListNode *reverse(ListNode *head) {
+        ListNode *prev = NULL, *curr = head;
+        while (curr) {
+            ListNode *temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+
+    ListNode *middle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        while (fast->next && fast->next->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        return slow;
+    }
+
+    bool isPalindrome(ListNode *head) {
+        ListNode *mid = middle(head);
+        mid->next = reverse(mid->next);
+        mid = mid->next;
+        while (mid) {
+            if (head->val != mid->val) return false;
+            head = head->next;
+            mid = mid->next;
+        }
+        return true;
+    }
+};

479cef373564a1e59828229515bb86641a62859c
Nikhil Nagrale <nknagrale634@gmail.com>
2021-07-10 00:30:19 +0530

    another approach using binary search added first search column then row

--- Medium/74.Search_a_2D_Matrix.cpp
+++ Medium/74.Search_a_2D_Matrix.cpp
@@ -41,3 +41,43 @@ class Solution {
         return matrix[r / m][r % m] == target;
     }
 };
+
+//  Binary Search
+//  Time Complexity - O(log(nm))
+//  Space Complexity - O(1)
+class Solution {
+   public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.size() == 0 || matrix[0].size() == 0) return false;
+
+        int low;
+        int high;
+
+        // first search in first column, use binary search
+        for (low = 0, high = matrix.size() - 1; low <= high;) {
+            int middle = low + (high - low) / 2;
+            if (matrix[middle][0] < target)
+                low = middle + 1;
+            else if (matrix[middle][0] > target)
+                high = middle - 1;
+            else
+                return true;
+        }
+
+        // when above loop ends, search in row[high]
+        int row = high;
+        if (row >= 0) {
+            for (low = 0, high = matrix[row].size() - 1; low <= high;) {
+                int middle = (low + high) / 2;
+                if (matrix[row][middle] < target)
+                    low = middle + 1;
+                else if (matrix[row][middle] > target)
+                    high = middle - 1;
+                else
+                    return true;
+            }
+        }
+
+        return false;
+    }
+};

bd57f66ff0d5ad78219417b466b6b340d5704efa
Nikhil Nagrale <nknagrale634@gmail.com>
2021-08-01 23:24:59 +0530

    minor changes

--- Easy/1.Two_Sum.cpp
+++ Easy/1.Two_Sum.cpp
@@ -13,17 +13,14 @@
 //  Time Complexity - O(n^2)  - TLE
 //  Space Complexity - O(1)
 //  Bruteforce Solution
-class Solution
-{
-public:
-    vector<int> twoSum(vector<int> &nums, int target)
-    {
+class Solution {
+   public:
+    vector<int> twoSum(vector<int> &nums, int target) {
         int n = nums.size();
         int i, j;
         for (i = 0; i < n; i++)
             for (j = i + 1; j < n; j++)
-                if (target - nums[i] == nums[j])
-                    return {i, j};
+                if (target - nums[i] == nums[j]) return {i, j};
         return {i, j};
     }
 };
@@ -31,17 +28,13 @@ public:
 //  Time Complexity - O(n)
 //  Space Complexity - O(n)
 //  Hashing Solution
-class Solution
-{
-public:
-    vector<int> twoSum(vector<int> &nums, int target)
-    {
+class Solution {
+   public:
+    vector<int> twoSum(vector<int> &nums, int target) {
         vector<int> ans;
         unordered_map<int, int> mp;
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (mp.find(target - nums[i]) != mp.end())
-            {
+        for (int i = 0; i < nums.size(); i++) {
+            if (mp.find(target - nums[i]) != mp.end()) {
                 ans.push_back(mp[target - nums[i]]);
                 ans.push_back(i);
                 return ans;
